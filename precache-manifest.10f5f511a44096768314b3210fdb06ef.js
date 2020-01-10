a347ecc7d66777e62974c02ecbc2a08f5d4f5720
carlhernek <53989735+carlhernek@users.noreply.github.com>
2020-01-10 16:25:09 +0100

    Initial commit

---
+++ precache-manifest.10f5f511a44096768314b3210fdb06ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd39b39d86469d3a4dfefa5c00179efd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbf06e408e15e5745db3",
+    "url": "/static/css/main.b87cd7c8.chunk.css"
+  },
+  {
+    "revision": "5eea8e1a02baf2b52281",
+    "url": "/static/js/2.23278c15.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.23278c15.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fbf06e408e15e5745db3",
+    "url": "/static/js/main.58ec925d.chunk.js"
+  },
+  {
+    "revision": "3de4d8d5072953609c9d",
+    "url": "/static/js/runtime-main.1856bfbd.js"
+  },
+  {
+    "revision": "c55d06a4a94199b7d8a5cee452780ba8",
+    "url": "/static/media/Billabong.c55d06a4.woff"
+  },
+  {
+    "revision": "7cf511abaf0b0abd0fc4f09852af70b2",
+    "url": "/static/media/arrow-right.7cf511ab.svg"
+  },
+  {
+    "revision": "d13fb8b43483d9535a4e8c9d6c3e2eac",
+    "url": "/static/media/heart-filled.d13fb8b4.svg"
+  },
+  {
+    "revision": "0dae4a2d4906b4fe9ba6eb03f3629b38",
+    "url": "/static/media/heart.0dae4a2d.svg"
+  }
+]);
